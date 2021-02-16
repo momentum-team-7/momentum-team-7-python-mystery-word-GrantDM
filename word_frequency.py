7096841b62594d2bfac54ae5ced294390a470e53
GrantDM <mullen.d.grant@gmail.com>
2021-02-16 18:33:53 -0500

    sorted and removed Stop words

--- word_frequency.py
+++ word_frequency.py
@@ -9,17 +9,35 @@ def print_word_freq(file):
     with open(file) as praise_song:
         words = praise_song.read()
         words = words.lower()
+        words_count_list = {}
+
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for ele in words:
+            if ele in punctuations:
+                words = words.replace(ele, "")
+                words = words.replace("\n", " ")
+
         words_list = words.split(' ')
         words_list_copy = words_list.copy()
-        for word in words_list_copy:
-            for stop in STOP_WORDS:
-                if stop == word:
-                    words_list_copy.remove(word)
-    print(words_list_copy)
+        for word in words_list:
+            if word in STOP_WORDS:
+                words_list_copy.remove(word)
+            elif word not in words_count_list:
+                temp_count = words_list_copy.count(word)
+                words_count_list[word] = temp_count
 
+        sorted_values = sorted(words_count_list.values(), reverse = True)
+        sorted_dict = {}
 
+        for index in sorted_values:
+            for key in words_count_list.keys():
+                if words_count_list[key] == index:
+                    sorted_dict[key] = words_count_list[key]
 
-
+        # words_count_list = sorted(words_count_list, key=words_count_list.get)
+        print(words_list_copy)
+        print(sorted_dict)
+        
     #pass
 
 
